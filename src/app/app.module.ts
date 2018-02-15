a4f102c0bd93a700a9eb8a45cfc7353298eba498
Edison Rojas <ediso-rojas@hotmail.es>
2018-02-15 17:03:23 -0500

    Creacion de os componentes del sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { NotasComponent } from './components/notas/notas.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    UsuarioComponent,
+    NotasComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule
